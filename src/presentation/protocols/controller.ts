6233684f54cce8c1d357649ea39a2cad98a4b5f6
Jordi Sabat <jsabat@gmail.com>
2020-10-14 14:29:01 +0200

    chore: add prettier configuration for auto format code

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,6 +1,5 @@
 import { HttpResponse, HttpResquest } from './http'
 
 export interface Controller {
-  // eslint-disable-next-line @typescript-eslint/method-signature-style
-  handle (httpRequest: HttpResquest): HttpResponse
+  handle: (httpRequest: HttpResquest) => HttpResponse
 }
