0f4a5267f5cd1dcf89ff14e19baf93039b4203e8
Jordi Sabat <jsabat@gmail.com>
2020-10-16 19:06:48 +0200

    refactor: call error with null as string on tests

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -172,7 +172,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError('error'))
+    expect(httpResponse.body).toEqual(new ServerError(null))
   })
 
   test('Should call AddAccount with correct values', async () => {
@@ -209,7 +209,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError('error'))
+    expect(httpResponse.body).toEqual(new ServerError(null))
   })
 
   test('Should return 200 if valid data is provided', async () => {
