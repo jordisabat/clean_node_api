8dfa9a81d8cb7cc88fe19c9db8882a7abb5bbd8a
Jordi Sabat <jsabat@gmail.com>
2020-11-08 18:11:57 +0100

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infrastructure/db/mongodb/account_repository/account_repository.ts
+++ src/infrastructure/db/mongodb/account_repository/account_repository.ts
@@ -1,12 +1,20 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add_account_repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load_account_by_email_repository'
 import { AccountModel } from '../../../../domain/models/account_model'
 import { AddAccountModel } from '../../../../domain/usecases/add_account'
 import { MongoHelper } from '../helpers/mongo_helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return MongoHelper.map(account)
   }
 }
