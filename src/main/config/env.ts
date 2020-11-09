939cc1e1a50edcb42ec92a779d608facfe570ce2
Jordi Sabat <jsabat@gmail.com>
2020-11-09 13:38:15 +0100

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean_node_api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecrete: process.env.JWT_SECRET || 't234KWsfd3'
 }
--- src/main/routes/login_routes_routes.ts
+++ src/main/routes/login_routes_routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express_route_adapter'
-import { makeSignUpController } from '../factories/signup/signup'
+import { makeSignUpController } from '../factories/signup/signup_factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
